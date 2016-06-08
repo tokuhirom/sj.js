71c62fe40ab9743ce98e79fa18a73932e1721b05
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-06-09 00:52:53 +0900

    oops

--- test-sj-expression.js
+++ test-sj-expression.js
@@ -1,8 +1,8 @@
-const e = require('./sj-expression');
+const e = require('./src/sj-expression');
 const assert = require('assert');
-assert.equal(e.sjExpression.getValueByPath({"x": "y"}, 'x'), 'y');
-assert.equal(e.sjExpression.getValueByPath({"x": {"y": "z"}}, 'x.y'), 'z');
-assert.equal(e.sjExpression.getValueByPath(
+assert.equal(e.getValueByPath({"x": "y"}, 'x'), 'y');
+assert.equal(e.getValueByPath({"x": {"y": "z"}}, 'x.y'), 'z');
+assert.equal(e.getValueByPath(
     {
       "x": () => {
         return 3
