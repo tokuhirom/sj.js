891957c850320a911a73736bab4710560d9bb2c0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-06-08 20:42:38 +0900

    sj-expression should support functaion call

--- test-sj-expression.js
+++ test-sj-expression.js
@@ -2,3 +2,9 @@ const e = require('./sj-expression');
 const assert = require('assert');
 assert.equal(e.sjExpression.getValueByPath({"x": "y"}, 'x'), 'y');
 assert.equal(e.sjExpression.getValueByPath({"x": {"y": "z"}}, 'x.y'), 'z');
+assert.equal(e.sjExpression.getValueByPath(
+    {
+      "x": () => {
+        return 3
+      }
+    }, 'x()'), 3);
