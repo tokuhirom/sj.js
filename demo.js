964b7397d5e7d1378eb29f7b0060c87a68308ec6
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-06-08 13:58:57 +0900

    added test case

--- demo.js
+++ demo.js
@@ -1,51 +1,151 @@
 class FooElement extends HTMLElementBase {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    createdCallback() {
-        super.createdCallback()
+  createdCallback() {
+    super.createdCallback()
 
-        this.innerHTML = `
-        <button sj-click="btnclick">yay</button>
+    this.innerHTML = `
 
         <hr>
 
+
+        <hr>
+
+        `;
+
+    this.scope.bar = "aaa";
+  }
+
+  btnclick(e) {
+    alert("Clicked");
+  }
+
+  set name(v) {
+    console.log(v);
+  }
+}
+customElements.define('x-foo', FooElement);
+
+customElements.define('test-events', class extends HTMLElementBase {
+  initialize() {
+    this.innerHTML = `
+        <button id="clickTest" sj-click="btnclick">yay</button>
+        `;
+  }
+
+  attachedCallback() {
+    console.log("GO");
+    let elem = document.getElementById("clickTest");
+    elem.click();
+  }
+
+  btnclick() {
+    this.clicked = true;
+  }
+
+  done_testing() {
+    return !!this.clicked;
+  }
+});
+
+customElements.define('test-input', class extends HTMLElementBase {
+  initialize() {
+    this.innerHTML = `
         <h1>Input</h1>
-        <input type="text" name="name" sj-model="name">
+        <input type="text" name="name" sj-model="name" id="myInput">
         Hello, <span sj-model="name"></span>
+        `;
+  }
 
-        <hr>
+  attachedCallback() {
+    this.querySelector('input').value = 'foo';
+    const input = this.querySelector('input');
+    input.dispatchEvent(new Event("change"));
+  }
 
+  done_testing() {
+    return this.querySelector('span').textContent === "foo";
+  }
+});
+
+customElements.define('test-textarea', class extends HTMLElementBase {
+  initialize() {
+    this.innerHTML = `
         <h1>Textarea</h1>
         <textarea name="hoge" sj-model="hoge"></textarea>
         Hello, <span sj-model="hoge"></span>
+        `;
+  }
 
-        <hr>
+  attachedCallback() {
+    const input = this.querySelector('textarea');
+    input.value = "foo";
+    input.dispatchEvent(new Event("change"));
+  }
 
+  done_testing() {
+    return this.querySelector('span').textContent === "foo";
+  }
+});
+
+customElements.define('test-from-controller', class extends HTMLElementBase {
+  initialize() {
+    this.innerHTML = `
         <h1>Passed from controller</h1>
         <input type="text" name="bar" sj-model="bar">
+    `;
+    this.scope.bar = "foo";
+  }
 
-        <hr>
+  attachedCallback() {
+  }
 
+  done_testing() {
+    return this.querySelector('input').value === "foo";
+  }
+});
+
+customElements.define('test-select', class extends HTMLElementBase {
+  initialize() {
+    this.innerHTML = `
         <h1>Select</h1>
         <select sj-model="sss">
             <option value="ppp">ppp</option>
             <option value="qqq">qqq</option>
         </select>
         SSS: <span sj-model="sss"></span>
-        `;
+    `;
+  }
 
-        this.scope.bar = "aaa";
+  done_testing() {
+    return this.querySelector('span').textContent === "ppp";
+  }
+});
+
+// test case runner
+window.addEventListener("load", function () {
+  let tests = document.getElementsByClassName("test");
+  let successCount = 0;
+  let failCount = 0;
+  for (let i = 0, l = tests.length; i < l; i++) {
+    const success = tests[i].done_testing();
+    if (success) {
+      successCount++;
+
+      tests[i].style.backgroundColor = "green";
+    } else {
+      failCount++;
+      tests[i].style.backgroundColor = "red";
     }
+  }
 
-    btnclick(e) {
-        alert("Clicked");
-    }
-
-    set name(v) {
-        console.log(v);
-    }
-}
-customElements.define('x-foo', FooElement);
-
+  const resultElem = document.getElementById("testResult");
+  resultElem.textContent = `Success: ${successCount} Fail: ${failCount} Total: ${tests.length}`;
+  if (successCount === tests.length) {
+    resultElem.style.color = "green";
+  } else {
+    resultElem.style.color = "red";
+  }
+});
--- sj.js
+++ sj.js
@@ -1,113 +1,117 @@
 (function (window) {
-    const sj_attr2event = {
-        'sj-click': 'click',
-        'sj-submit':'submit'
-    };
+  const sj_attr2event = {
+    'sj-click': 'click',
+    'sj-submit': 'submit'
+  };
 
-    function isFormElement(elem) {
-        return elem instanceof HTMLInputElement
-            || elem instanceof HTMLTextAreaElement
-            || elem instanceof HTMLSelectElement;
+  function isFormElement(elem) {
+    return elem instanceof HTMLInputElement
+           || elem instanceof HTMLTextAreaElement
+           || elem instanceof HTMLSelectElement;
+  }
+
+  class HTMLElementBase extends HTMLElement {
+    createdCallback() {
+      this.scope = new Proxy({}, {
+        set: (target, property, value, receiver) => {
+          target[property] = value;
+
+          if (!this.scanned) {
+            this.feedbackQueue.push(property);
+          } else {
+            this.feedbackToUI(property, value);
+          }
+          return true;
+        }
+      });
+      this.scanned = false; // already scanned
+      this.feedbackQueue = []; // queue
+      this.model2elements = {};
+
+      this.initialize();
+      this.go();
     }
 
-    class HTMLElementBase extends HTMLElement {
-        createdCallback() {
-            this.scope = new Proxy({}, {
-                set: (target, property, value, receiver) => {
-                    target[property] = value;
-
-                    if (!this.scanned) {
-                        this.feedbackQueue.push(property);
-                    } else {
-                        this.feedbackToUI(property, value);
-                    }
-                    return true;
-                }
-            });
-            this.scanned = false; // already scanned
-            this.feedbackQueue = []; // queue
-            this.model2elements = {};
-
-            let observer = new MutationObserver(mutations => {
-                this.scanElements();
-                if (!this.scanned) {
-                    for (let property of this.feedbackQueue) {
-                        this.feedbackToUI(property, this.scope[property]);
-                    }
-
-                    this.feedbackQueue = [];
-                    this.scanned = true;
-                }
-            });
-            observer.observe(this, {
-                childList: true,
-                characterData: true
-            });
-        }
-
-        scanElements() {
-            let children = this.querySelectorAll("*");
-            for (let i = 0; i < children.length; ++i) {
-                let child = children[i];
-                let attrs = child.attributes;
-                for(let j = 0; j<attrs.length; j++) {
-                    let attr = attrs[j];
-                    if (attr.name.startsWith('sj-')) {
-                        let event = sj_attr2event[attr.name];
-                        if (event) {
-                            this.registerListener(child, event, attr.value);
-                        } else if (attr.name == 'sj-model') {
-                            this.bindModel(child, attr.value);
-                        }
-                    }
-                }
-            }
-        }
-
-        registerListener(elem, name, callback) {
-            elem.addEventListener(name, e => {
-                this[callback](e);
-            });
-        }
-
-        bindModel(elem, name) {
-            if (!this.model2elements[name]) {
-                this.model2elements[name] = new Set();
-            }
-            this.model2elements[name].add(elem);
-
-            if (
-                   elem instanceof HTMLInputElement
-                || elem instanceof HTMLTextAreaElement
-                || elem instanceof HTMLSelectElement
-            ) {
-                if (!this.scope[name]) {
-                    this.scope[name] = elem.value;
-                }
-                elem.addEventListener('change', e => {
-                    this.scope[name] = elem.value;
-                });
-            }
-        }
-
-        feedbackToUI(property, value) {
-            let elems = this.model2elements[property];
-            if (elems) {
-                for (let elem of elems.keys()) {
-                    this.feedbackToElement(elem, value);
-                }
-            }
-        }
-
-        feedbackToElement(elem, value) {
-            if (isFormElement(elem)) {
-                elem.value = value;
-            } else if (elem instanceof HTMLSpanElement) {
-                elem.textContent = value;
-            }
-        }
-
+    initialize() {
+      // nop. abstract method.
     }
-    window.HTMLElementBase = HTMLElementBase;
+
+    go() {
+      console.log(`scanning ${this.tagName}`);
+      this.scanElements();
+      if (!this.scanned) {
+        for (const property of this.feedbackQueue) {
+          this.feedbackToUI(property, this.scope[property]);
+        }
+
+        this.feedbackQueue = [];
+        this.scanned = true;
+      }
+    }
+
+    scanElements() {
+      let children = this.querySelectorAll("*");
+      for (let i = 0; i < children.length; ++i) {
+        let child = children[i];
+        let attrs = child.attributes;
+        for (let j = 0; j < attrs.length; j++) {
+          let attr = attrs[j];
+          if (attr.name.startsWith('sj-')) {
+            let event = sj_attr2event[attr.name];
+            if (event) {
+              this.registerListener(child, event, attr.value);
+            } else if (attr.name == 'sj-model') {
+              this.bindModel(child, attr.value);
+            }
+          }
+        }
+      }
+    }
+
+    registerListener(elem, name, callback) {
+      elem.addEventListener(name, e => {
+        this[callback](e);
+      });
+    }
+
+    bindModel(elem, name) {
+      if (!this.model2elements[name]) {
+        this.model2elements[name] = new Set();
+      }
+      this.model2elements[name].add(elem);
+
+      if (
+          elem instanceof HTMLInputElement
+          || elem instanceof HTMLTextAreaElement
+          || elem instanceof HTMLSelectElement
+      ) {
+        if (!this.scope[name]) {
+          this.scope[name] = elem.value;
+        }
+        elem.addEventListener('change', e => {
+          this.scope[name] = elem.value;
+        });
+      }
+    }
+
+    feedbackToUI(property, value) {
+      let elems = this.model2elements[property];
+      if (elems) {
+        for (let elem of elems.keys()) {
+          this.feedbackToElement(elem, value);
+        }
+      }
+    }
+
+    feedbackToElement(elem, value) {
+      if (isFormElement(elem)) {
+        elem.value = value;
+      } else if (elem instanceof HTMLSpanElement) {
+        elem.textContent = value;
+      }
+    }
+
+  }
+  window.HTMLElementBase = HTMLElementBase;
 
 })(this);
