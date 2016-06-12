95f96815433089351aa0591236ecea8900fe9d2a
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-06-12 22:02:21 +0900

    remove expression runner

--- test/test-aggregator.js
+++ test/test-aggregator.js
@@ -1,15 +1,12 @@
 const jsdom = require('jsdom');
 const test = require('tape');
 const SJAggregator = require('../src/default-value-aggregator');
-const ExpressionRunner = require('../src/expression-runner.js');
-
-const expressionRunner = new ExpressionRunner();
 
 test('input', (t) => {
   t.plan(1);
   const e = jsdom.jsdom('<input sj-model="this.hoge" value="iyan">');
   const scope = {};
-  new SJAggregator(e, expressionRunner).aggregate(scope);
+  new SJAggregator(e).aggregate(scope);
   t.deepEqual(scope, {
     hoge: 'iyan'
   });
@@ -19,7 +16,7 @@ test('textarea', (t) => {
   t.plan(1);
   const e = jsdom.jsdom('<textarea sj-model="this.hoge">iyan</textarea>');
   const scope = {};
-  new SJAggregator(e, expressionRunner).aggregate(scope);
+  new SJAggregator(e).aggregate(scope);
   t.deepEqual(scope, {
     hoge: 'iyan'
   });
@@ -29,7 +26,7 @@ test('select', (t) => {
   t.plan(1);
   const e = jsdom.jsdom('<select sj-model="this.hoge"><option value="iyan" checked>iyan</option></select>');
   const scope = {};
-  new SJAggregator(e, expressionRunner).aggregate(scope);
+  new SJAggregator(e).aggregate(scope);
   t.deepEqual(scope, {
     hoge: 'iyan'
   });
