852a17ec28cc0bf82f137a3fd0b48c96fa90c48b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-06-14 19:31:49 +0900

    autoload sj-app

--- src/main.js
+++ src/main.js
@@ -8,3 +8,17 @@ const Element = require('./element.js');
 
 module.exports.Element = Element;
 module.exports.tag = tag;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const elems = document.querySelectorAll('[sj-app]');
+  for (let i=0, l=elems.length; i<l; ++i) {
+    const elem = elems[i];
+    const template = document.createElement("sj-template");
+    template.innerHTML = elem.innerHTML;
+    elem.parentNode.replaceChild(template, elem);
+  }
+  customElements.define('sj-template', class extends sj.Element {
+    template() { return this.innerHTML; }
+    initialize() { }
+  });
+});
